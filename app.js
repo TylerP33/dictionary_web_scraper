5668dffcfdbeee1e04c2007ed873cc5e4b070681
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-09-26 02:04:12 -0500

    Final Commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post('/search', [
 ], (req, res) => {
 	  const errors = validationResult(req);
   		if (!errors.isEmpty()) {
-    		return res.status(422).json({ errors: errors.array() });
+    		res.render('error')
   		}
 	var keyword = req.body.keyword
 	res.redirect('/search/' + keyword);
